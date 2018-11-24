7c5aa481e71f25ea7409a8e097b4a3a2c35816c9
sintaxd <jasintaxd@yahoo.com>
2018-11-24 10:57:09 +0700

    make migration files

---
+++ database/migrations/2018_11_24_035108_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() 
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id_user');
+            $table->string('username');
+            $table->string('password');
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('no_hp');
+            $table->string('asal_sekolah');
+            $table->string('email')->unique();
+            $table->integer('verif_email')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
